139d2340cdd548f7988fae8527fb7f1c728b4997
Dahoon Song <dhsong@berkeley.edu>
2019-03-15 13:58:54 -0700

    first commit

---
+++ app/src/main/java/com/example/cs160_sp18/prog3/Login_Page.java
@@ -0,0 +1,25 @@
+package com.example.cs160_sp18.prog3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Login_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page_layout);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
